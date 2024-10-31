cc3d2b3b3dc3d00117c022517b09e9f7890991cb
sslanga <160239967+sslanga@users.noreply.github.com>
2024-10-31 13:09:09 -0400

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -1,13 +1,15 @@
 const cart = [];
 
-const addToCartButtons = document.querySelectorAll("add-to-cart");
-addToCartButtons.forEach((button, index) => {
+const addToCartButtons = document.querySelectorAll(".add-to-cart");
+
+addToCartButtons.forEach((button) => {
   button.addEventListener("click", () => {
     const productName = button.parentElement.querySelector("h2").textContent;
     const productPrice = button.parentElement.querySelector("p").textContent;
 
     cart.push({ name: productName, price: productPrice });
     console.log(cart);
-    alert("${productName} is added to your cart");
+    alert(`${productName} is added to your cart`);
   });
 });
+
